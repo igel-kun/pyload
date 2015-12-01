7ee3e9b91242ee1d084b9baf5a7287b048799249
Igel <ich.freak@gmx.de>
2015-12-01 13:38:26 +0100

    XFileSharing keepalive

--- module/plugins/hooks/XFileSharing.py
+++ module/plugins/hooks/XFileSharing.py
@@ -35,12 +35,12 @@ class XFileSharing(Addon):
                         "fileparadox.in", "filevice.com", "hostingbulk.com", "junkyvideo.com",
                         "ravishare.com", "salefiles.com", "sendmyway.com", "sharebeast.com",
                         "sharesix.com", "thefile.me", "verzend.be", "worldbytez.com",
-                        "xvidstage.com",
+                        "xvidstage.com", "linestorage.com",
                         # NOT TESTED:
                         "101shared.com", "4upfiles.com", "filemaze.ws", "filenuke.com",
                         "linkzhost.com", "mightyupload.com", "rockdizfile.com", "sharerepo.com",
                         "shareswift.com", "uploadbaz.com", "uploadc.com", "vidbull.com",
-                        "zalaa.com", "zomgupload.com",
+                        "zalaa.com", "zomgupload.com", "movdivx.com",
                         # NOT WORKING:
                         "amonshare.com", "banicrazy.info", "boosterking.com", "host4desi.com",
                         "laoupload.com", "rd-fs.com"]
