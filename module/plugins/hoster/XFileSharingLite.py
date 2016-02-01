b8bd82eeab62ef133b2bfb683d90b0e8d46c69a8
Igel <ich.freak@gmx.de>
2016-02-01 11:20:19 +0100

    updated XFileSharing Lite

--- module/plugins/hoster/XFileSharingLite.py
+++ module/plugins/hoster/XFileSharingLite.py
@@ -5,13 +5,13 @@ from module.plugins.hoster.XFileSharing import XFileSharing, create_getInfo
 class XFileSharingLite(XFileSharing):
     __name__ = "XFileSharingLite"
     __type__ = "hoster"
-    __pattern__ = r"http://(?:\w*\.)*(?P<DOMAIN>vodlocker.com|played.to|faststream.in)/\w{12}"
+    __pattern__ = r"http://(?:\w*\.)*(?P<DOMAIN>vodlocker\.com|vodlockers\.tk|played\.to|faststream\.in)/\w{12}"
     __version__ = "0.01"
     __description__ = """XFileSharingLite plugin"""
     __author_name__ = ("igel")
     __author_mail__ = ("")
 
-    WAIT_TIME = r'var countdownNum = (\d+)'
+    WAIT_TIME = r'countdown[^0-9]*(\d+)'
     # if called without DOTALL, we need this:
     #LINK_PATTERN = r'jwplayer(?:.|\n)*?file: "([^"]*)"'
     # with DOTALL, do this:
