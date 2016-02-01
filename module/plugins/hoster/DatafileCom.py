b6ab749419edbe6b22f020c4b56be92d0ce0b02c
Igel <ich.freak@gmx.de>
2016-02-01 11:18:34 +0100

    update Datafile

--- module/plugins/hoster/DatafileCom.py
+++ module/plugins/hoster/DatafileCom.py
@@ -3,6 +3,7 @@
 import pycurl
 import re
 import time
+from urlparse import urlparse
 
 from module.common.json_layer import json_loads
 from module.plugins.captcha.ReCaptcha import ReCaptcha
@@ -14,55 +15,76 @@ class DatafileCom(SimpleHoster):
     __type__    = "hoster"
     __version__ = "0.01"
 
-    __pattern__ = r'https?://(?:www\.)?(?:datafile|wcrypt)\.com/.*'
-    __description__ = """Rapidu.net hoster plugin"""
+    __pattern__ = r'https?://(?:www\.)?(?:datafile|wcrypt)\.com/\w*/(?P<ID>\w+)'
+    __description__ = """Datafile & Wcrypt hoster plugin"""
     __license__     = "GPLv3"
-    __authors__     = [("prOq", "")]
+    __authors__     = [("prOq", ""),
+                       ("igel", "")]
 
 
     INFO_PATTERN    = r'<div class="file-name">(?P<N>.*?)</div>.*<div class="file-size">Filesize: .*?(?P<S>[\d.]+)\s(?P<U>\w+)</'
     OFFLINE_PATTERN = r'<h1>404'
     ERROR_PATTERN = r'<div class="error">'
-    RECAPTCHA_KEY_PATTERN = r'"https?://www.google.com/recaptcha/api/challenge?k=(.*?)">'
-    PREMIUM_ONLY_PATTERN  = r'downloaded only by users with Premium'
+    PREMIUM_ONLY_PATTERN  = r'can be downloaded only by users.*Premium account'
+    WAIT_TIME = r'exceeded your free.*download limit'
+    DELAY_PATTERN = r"counter.contdownTimer\('(?P<time>\d+)'"
+    DL_LIMIT_PATTERN = r'You are downloading another file at this moment.'
 
     def setup(self):
         self.resumeDownload = True
         self.multiDL        = self.premium
 
-
     def handle_free(self, pyfile):
+        self.check_errors()
         self.req.http.lastURL = pyfile.url
         self.req.http.c.setopt(pycurl.HTTPHEADER, ["X-Requested-With: XMLHttpRequest"])
 
-        jsvars = self.getJsonResponse("https://rapidu.net/ajax.php",
-                                      get={'a': "getLoadTimeToDownload"},
-                                      post={'_go': ""},
-                                      decode=True)
-
-        if str(jsvars['timeToDownload']) is "stop":
-            t = (24 * 60 * 60) - (int(time.time()) % (24 * 60 * 60)) + time.altzone
-
-            self.log_info("You've reach your daily download transfer")
-
-            self.retry(10, 10 if t < 1 else None, _("Try tomorrow again"))  #@NOTE: check t in case of not synchronised clock
-
+        parse = urlparse(pyfile.url)
+        base = parse.scheme + '://' + parse.netloc
+        ajax_target = base + '/files/ajax.html'
+        
+        m = re.search(self.DELAY_PATTERN, self.data)
+        if m is not None:
+            delay = m.group('time')
         else:
-            self.wait(int(jsvars['timeToDownload']) - int(time.time()))
-
+            # default delay to 2min
+            delay = 120
+        
+        m = re.search(r'google.com/recaptcha/api/challenge.*"', self.data)
+        self.log_debug('found captcha: %s' % m.group(0))
+        
         recaptcha = ReCaptcha(self)
-        response, challenge = recaptcha.challenge(self.RECAPTCHA_KEY)
+        response, challenge = recaptcha.challenge()
 
-        jsvars = self.getJsonResponse("https://rapidu.net/ajax.php",
-                                      get={'a': "getCheckCaptcha"},
-                                      post={'_go'     : "",
-                                            'captcha1': challenge,
-                                            'captcha2': response,
-                                            'fileId'  : self.info['pattern']['ID']},
+        jsvars = self.getJsonResponse(ajax_target,
+                                      post={'doaction' : "validateCaptcha",
+                                            'recaptcha_challenge_field': challenge,
+                                            'recaptcha_response_field': response,
+                                            'fileid'  : self.info['pattern']['ID']},
+                                      decode=True) 
+
+        
+        if jsvars['success'] == 1:
+            token = jsvars['token']
+            self.captcha.correct()
+        else:
+            self.retry_captcha()
+        
+        self.log_debug('captcha was good, got token %s, now waiting %s seconds' % (str(token), str(delay)) )
+        self.wait(delay)
+ 
+        jsvars = self.getJsonResponse(ajax_target,
+                                      post={'doaction' : "getFileDownloadLink",
+                                            'recaptcha_challenge_field': challenge,
+                                            'recaptcha_response_field': response,
+                                            'token' : token,
+                                            'fileid' : self.info['pattern']['ID']},
                                       decode=True)
-
-        if jsvars['message'] == 'success':
-            self.link = jsvars['url']
+        
+        if 'success' in jsvars and jsvars['success'] == 1:
+            self.link = jsvars['link']
+        else:
+            self.error(_("Free download link not found"))
 
 
     def getJsonResponse(self, *args, **kwargs):
