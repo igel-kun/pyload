d12283a420ae27e53754bcff023ac61fb8e46c12
Igel <ich.freak@gmx.de>
2016-08-30 12:49:00 +0200

    fix reconnect issues, especially when logged in with a free account

--- module/plugins/internal/Base.py
+++ module/plugins/internal/Base.py
@@ -353,12 +353,7 @@ class Base(Plugin):
 
         self.log_info(_("Waiting %s...") % format_time(wait_time))
 
-        if self.wantReconnect:
-            self.log_info(_("Requiring reconnection..."))
-            if self.account:
-                self.log_warning(_("Reconnection ignored due logged account"))
-
-        if not self.wantReconnect or self.account:
+        if not self.wantReconnect:
             while self.pyfile.waitUntil > time.time():
                 self.check_status()
                 time.sleep(2)
@@ -373,9 +368,9 @@ class Base(Plugin):
                     self.wantReconnect = False
 
                     self.req.clearCookies()
-                    raise Reconnect
-
+                    break
                 time.sleep(2)
+            raise Reconnect
 
         self.waiting = False
         self.pyfile.status = status  #@NOTE: Recheck in 0.4.10
