66ba677d89e47ea6273977d8b9293de92a62818c
Igel <ich.freak@gmx.de>
2017-08-31 15:45:03 +0200

    minor fixes

--- module/plugins/hoster/TezfilesCom.py
+++ module/plugins/hoster/TezfilesCom.py
@@ -5,13 +5,16 @@ from module.plugins.hoster.Keep2ShareCc import Keep2ShareCc
 class TezfilesCom(Keep2ShareCc):
     __name__    = "TezfilesCom"
     __type__    = "hoster"
-    __version__ = "0.01"
+    __version__ = "0.02"
     __status__  = "testing"
 
     __pattern__ = r'https?://(?:www\.)?tezfiles\.com/file/(?P<ID>\w+)'
 
     __description__ = """Tezfiles.com hoster plugin"""
     __license__     = "GPLv3"
-    __authors__     = [("igel", "")]
+    __authors__     = [("igel", None)]
+
+
+    API_URL = "http://tezfiles.com/api/v2/"
 
 
