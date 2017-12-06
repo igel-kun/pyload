e63e2b54e7eef1f58005db28a63478cc26ba5c6c
Igel <ich.freak@gmx.de>
2017-12-06 14:46:05 +0000

    if a URL is not recognized, but redirects to something recognized, we need to re-add it to the package

--- module/plugins/hoster/Http.py
+++ module/plugins/hoster/Http.py
@@ -20,6 +20,9 @@ class Http(Hoster):
     __license__ = "GPLv3"
     __authors__ = [("Walter Purcaro", "vuolter@gmail.com")]
 
+    def noop(self):
+        pass
+
     def setup(self):
         self.chunk_limit = -1
         self.resume_download = True
@@ -27,6 +30,24 @@ class Http(Hoster):
     def process(self, pyfile):
         url = re.sub(r'^(jd|py)', "http", pyfile.url)
         netloc = urlparse.urlparse(url).netloc
+
+        # if we encounter a redicrect, add the new location as new link in the package and exit
+        loc = None
+        header = self.load(url, just_header=True, redirect=False)
+        if 'location' in header:
+            # follow all redirects
+            while True:
+                loc = header['location']
+                self.log_info(_('following redirect to ') + loc)
+                header = self.load(loc, just_header=True, redirect=False)
+                if not 'location' in header:
+                    break
+            self.log_info(_('adding %s as new item in the package' % loc))
+            self.pyload.api.addFiles(pyfile.package().id, [loc])
+            # if we don't download anything, we cannot check it
+            self._check_download = self.noop
+            self.check_duplicates = self.noop
+            return
 
         for _i in range(2):
             try:
