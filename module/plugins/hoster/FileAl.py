a385f39d7e8300963d24fd2d0a473ab3fd9fdc36
Igel <ich.freak@gmx.de>
2020-11-08 20:16:51 +0100

    small corrections to FileAl

--- module/plugins/hoster/FileAl.py
+++ module/plugins/hoster/FileAl.py
@@ -9,7 +9,7 @@ import re
 class FileAl(XFSHoster):
     __name__    = "FileAl"
     __type__    = "hoster"
-    __version__ = "0.01"
+    __version__ = "0.02"
     __status__  = "testing"
 
     __pattern__ = r'https?://(?:www\.)?file\.al/\w{12}'
@@ -24,11 +24,12 @@ class FileAl(XFSHoster):
     INFO_PATTERN = r'You have requested.*https?://[^<]*?(?P<N>[^/<]*)</.*?\((?P<S>[0-9.]* [A-Z]?B)'
     RECAPTCHA_PATTERN = r"g-recaptcha.*?sitekey=[\"']([^\"]*)"
     PREMIUM_ONLY_PATTERN  = r'(?:[Pp]remium Users only|can download files up to.*only)'
+    DL_LIMIT_PATTERN = r'You have reached the download-limit:[^<]*'
 
     #LOCAL_LOCATION = r'check_ip'
     #PRE_CAPTCHA_PATTERN = r"confirm that you're not a robot"
 
     def setup(self):
         self.multiDL = self.premium
-        self.resume_download = True
+        self.resume_download = self.premium
 
